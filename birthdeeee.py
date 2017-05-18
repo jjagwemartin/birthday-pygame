3be56a649b5126787c582fe9647d0ba003d63366
jjagwemartin <williamnkalubo@gmail.com>
2017-05-18 04:21:51 -0700

    Update birthdeeee.py

--- birthdeeee.py
+++ birthdeeee.py
@@ -5,17 +5,18 @@
 # birthday-pygame
 #a program that gets information from the user and generates his/her birthday information
 #this code is programed from python version 3.6.0
-a=input('input your first name : ')
-b=input('input your last name : ')
-c=input('when is your birth day (1-31)? : ')
-d=input('in which month (1-12)? : ')
-e=input('how old are u going to be ? : ')
-yob=2017-(int(e))
-months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-f=months[(int(d))-1]
-days=['st','nd','rd','th','th','th','th','th','th','th','th','th','th','th','th','th','th','th','th','th','st','nd','rd','th','th','th','th','th','th','th','st']
-g=days[(int(c))-1]
-
 import datetime
-h=datetime.date(int(yob),int(d),int(c))
-print('your name is' ,a, ' ' ,b, 'and you were born on' ,c+g, ' ' ,f, ' ' ,yob, 'the week day was a' ,h.strftime('%a'))
+firstname=input('What is  your first name : ')
+surname=input('What is your surname : ')
+day=input('When is your birth day (1-31)? : ')
+monthnumber=input('Its in which month (1-12)? : ')
+age=input('How old will you be ? : ')
+yob=2017-(int(age))
+months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+monthname=months[(int(monthnumber))-1]
+days=['st','nd','rd']+(['th']*17)+['st','nd','rd']+(['th']*7) + ['st']
+dayending=days[(int(day))-1]
+h=datetime.date(int(yob),int(monthnumber),int(day))
+print('your name is' ,firstname, ' ' ,surname, 'and you were born on' ,day+dayending, ' ' ,monthname, ' ' ,yob, 'the week day was a' ,h.strftime('%a'))
+
+
